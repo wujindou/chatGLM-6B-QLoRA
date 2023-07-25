520d2176d092a490aa47284c2d3c8448c4373931
JackieWu <wujindou1@163.com>
2023-07-25 16:01:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ nohup python3 train_qlora.py \
 --model_name_or_path THUDM/chatglm2-6b \
 --train_data_path data/train_cpm_0724.jsonl \
 --eval_data_path data/dev_cpm_0724.jsonl \
---lora_rank 4 \
+--lora_rank 16 \
 --lora_dropout 0.05 \
 --compute_dtype fp16 & 
